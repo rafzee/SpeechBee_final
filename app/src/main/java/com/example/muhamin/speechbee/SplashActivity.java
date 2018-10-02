f8937ba82cd7ff265ff7d0c11e3be9d8b870e5cf
rafzee <muhaiminbinmunir@gmail.com>
2018-10-02 15:22:43 +0600

    Error free version

--- app/src/main/java/com/example/muhamin/speechbee/SplashActivity.java
+++ app/src/main/java/com/example/muhamin/speechbee/SplashActivity.java
@@ -4,20 +4,32 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_TIME = 1500;
-
+    private TextView tv1, tv2, tv3;
+    private ImageView im;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        tv1 = findViewById(R.id.lets);
+        tv2 = findViewById(R.id.from);
+        tv3 = findViewById(R.id.speech);
+        im = findViewById(R.id.bee);
+        Log.d("haymen", "slpash_1");
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                Log.d("haymen", "slpash");
                 startActivity(i);
                 finish();
             }
