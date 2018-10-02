d1d87d54d749c0fb47a9df60fd5da25fdc16d6db
rafzee <muhaiminbinmunir@gmail.com>
2018-10-02 19:05:23 +0600

    nav partial startup fixed

--- app/src/main/java/com/example/muhamin/speechbee/TherapyLevelActivity.java
+++ app/src/main/java/com/example/muhamin/speechbee/TherapyLevelActivity.java
@@ -7,20 +7,28 @@ import android.view.View;
 import android.widget.Button;
 
 public class TherapyLevelActivity extends AppCompatActivity {
- Button b3;
+ Button b3, b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_therapy_level);
 
 
+
+        b1 = findViewById(R.id.btn1);
         b3=findViewById(R.id.btn3);
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(TherapyLevelActivity.this, ReceptiveLevelsActivity.class);
                 startActivity(intent);
+                }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TherapyLevelActivity.this, OralActivity.class);
             }
-    });
+        });
     }
 }
