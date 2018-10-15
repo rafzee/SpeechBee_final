fea9245e18baf3ae7a2838ba4a93f158beaa2caf
rafzee <muhaiminbinmunir@gmail.com>
2018-10-15 16:55:19 +0600

    minor changes

---
+++ app/src/main/java/com/example/muhamin/speechbee/OmeActivity.java
@@ -0,0 +1,34 @@
+package com.example.muhamin.speechbee;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OmeActivity extends AppCompatActivity {
+
+    private Button b1, b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ome);
+
+        b1 = findViewById(R.id.btn1);
+        b2 = findViewById(R.id.btn2);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), OralActivity.class));
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(getApplicationContext(), OralActivity.class));
+            }
+        });
+    }
+}
